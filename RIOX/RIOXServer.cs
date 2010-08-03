88c1e0314c8c9eeaea0c94047eeef6bd20466048
krisp <krisp@krisp.com>
2010-08-02 21:28:18 -0400

    Added GPL text

--- RIOX/RIOXServer.cs
+++ RIOX/RIOXServer.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* This file is part of RIOX.
+   RIOX is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   MiniDeluxe is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with RIOX.  If not, see <http://www.gnu.org/licenses/>.
+   
+   RIOX is Copyright (C) 2010 by K1FSY
+*/
+
+using System;
 using System.Collections;
 using System.Net.Sockets;
 using System.Net;
